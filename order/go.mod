33a821c4a10065464f3fb5d6ec27a0f96ed471e9
Pramod Singh Bisht -X (prambish - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prambish@cisco.com>
2023-02-15 15:51:15 +0530

    initial implementation for Order service

--- order/go.mod
+++ order/go.mod
@@ -3,9 +3,20 @@ module github.com/bishtpramod19/microservices/order
 go 1.19
 
 require (
+	github.com/bishtpramod19/microservices-proto/golang/order v0.0.0-20230215094542-299afb5ac4f8
+	gorm.io/driver/mysql v1.4.5
+	gorm.io/gorm v1.24.5
+)
+
+require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/driver/mysql v1.4.5 // indirect
-	gorm.io/gorm v1.24.5 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ order/internal/adapters/grpc/server.go
@@ -0,0 +1,39 @@
+package grpc
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/bishtpramod19/microservices-proto/golang/order"
+	"github.com/bishtpramod19/microservices/order/config"
+	"github.com/bishtpramod19/microservices/order/internal/ports"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type Adapter struct {
+	api  ports.ApiPort
+	port int
+	order.UnimplementedOrderServer
+}
+
+func NewAdapter(api ports.ApiPort, port int) *Adapter {
+	return &Adapter{api: api, port: port}
+}
+
+func (a Adapter) Run() {
+	var err error
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		log.Fatalf("failed to listen on port %d, error: %v", a.port, err)
+	}
+	grpcServer := grpc.NewServer()
+	order.RegisterOrderServer(grpcServer, a)
+	if config.GetEnv() == "development" {
+		reflection.Register(grpcServer)
+	}
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve grpc on port ")
+	}
+}
